9838c68283f9246d8669ce5f8c025054d2d3bfb3
Hapince <133777386+Cong-zz1@users.noreply.github.com>
2024-08-29 18:14:14 +0800

    11

--- app.py
+++ app.py
@@ -52,5 +52,5 @@ if __name__ == "__main__":
     if check_password():
         main()
     else:
-        st.warning("由于服务器资源有限，为避免不必要流量，请关注微信公众号“Hapince出海日记”获取密码")
+        st.warning("由于服务器资源有限，为避免不必要流量，请进入官方群或关注微信公众号“Hapince出海日记”获取密码")
         st.image("image/wechatgroup.jpg")
