82600668d1230e84d98113d75203ef9ca76d3cb5
Hapince <133777386+Cong-zz1@users.noreply.github.com>
2024-08-30 18:06:57 +0800

    11

--- app.py
+++ app.py
@@ -34,7 +34,6 @@ def fetch_all_results(search_engine, keyword, amazon_site, max_pages=10):
 
 def main():
     st.title("亚马逊僵尸链接查询工具 - （试用版本）")
-    st.write("添加微信“happy_prince45获取全功能僵尸链接采集软件”。1.查询链接条数无限制2.查询结果包含品牌评分3.可以一件导出asin4.多站点查询.......")
     st.subheader("搜索设置")
     search_engine = st.selectbox("选择搜索引擎", ["Google", "Bing"])
     amazon_site = st.selectbox("选择亚马逊站点", [
