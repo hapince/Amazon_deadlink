aa8989f58477cf847f218495621e28037c0f026e
Hapince <133777386+Cong-zz1@users.noreply.github.com>
2024-08-31 01:23:02 +0800

    1

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def main():
                     asin = extract_asin(link)
                     image_url = extract_image_url(asin) if asin else None
                     # Use default image if no image found
-                    image_tag = f'<img src="{image_url}" style="width:100px;height:100px;object-fit:cover;"/>' if image_url else f'<img src="image/logo.jpg" style="width:100px;height:100px;object-fit:cover;"/>'
+                    image_tag = f'<img src="{image_url}" style="width:100px;height:100px;object-fit:cover;"/>' if image_url else f'<img src="https://ninjify.shop/wp-content/uploads/2024/08/cd8c49fbc38ff9698eeb372638a3e8a.jpg" style="width:100px;height:100px;object-fit:cover;"/>'
                     st.session_state.results.append({"Image": image_tag, "Title": title, "URL": link, "ASIN": asin})
 
                 st.subheader(f"搜索结果-试用版限制{max_links}条，如果要取消限制，请联系管理员")
